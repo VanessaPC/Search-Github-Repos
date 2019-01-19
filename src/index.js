2d9b082428a879c164241c7821b78e0a4d2a8048
Vanessa Pitchford Cristobal <rc.vanessa87@gmail.com>
2019-01-19 15:28:49 +0200

    Adding all the packages I think I'll need and setting the httpcalls and tokens

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
+const TOKEN = "a1dd711e8d1ade34787e304c65d03394bc7cd0f5";
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: TOKEN ? `Bearer ${TOKEN}` : ""
+    }
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql"
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
